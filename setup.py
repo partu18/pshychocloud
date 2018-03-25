bab6fc2e8e00eadf19b4f5555767c627625c897a
partu18 <artusopablo@gmail.com>
2018-03-25 15:29:20 +0200

    Initial setup.py. For some reason isn't working

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='pshychocloud',
+      version='1.0',
+      description='Message analyzer and word cloud generator',
+      author='Ariel Cessario & Pablo Artuso',
+      packages=find_packages()
+     )
